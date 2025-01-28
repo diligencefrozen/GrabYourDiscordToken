bc241a9a1dcf1abda96035d1700db38ea79e3552
Frozen <152419439+diligencefrozen@users.noreply.github.com>
2025-01-28 17:49:01 +0900

    Create MyDiscordToken.js

---
+++ MyDiscordToken.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+
+// @name               Grab Your Discord Token
+
+// @description        Easily grab your Discord token with this script.
+
+// @author             diligencefrozen
+
+// @version            0128.2025
+
+// @match              *://*.discord.com/*
+
+// @icon               
+
+// ==/UserScript==
+
+(async () => {
+
+    let token = localStorage.getItem("token").replace(/"/g, "");
+
+
+    if (confirm("Would you like to grab your Discord token?")) {
+
+        prompt("Your Discord token:", token);
+    }
+
+
+    token = "";
+  
+})();
